4553736fab7829224faf62589b57e954237a370d
defi3 <pi@space-pi.com>
2024-07-01 12:36:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
-process.env["REACT_APP_BASEURL"] = "https://bscpage.space-pi.com";
+// process.env["REACT_APP_BASEURL"] = "https://bscpage.space-pi.com";
+process.env["REACT_APP_BASEURL"] = "";
 process.env["API_URL"] = "https://bsc.space-pi.com/api";
 
 // https://vitejs.dev/config/
